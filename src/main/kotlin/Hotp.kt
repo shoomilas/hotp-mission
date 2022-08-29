4e91ffcd7ca61481164139bf205d5e7401b61cc4
Jakub Szumilas <34002117+shoomilas@users.noreply.github.com>
2022-08-29 10:59:18 +0200

    Init CryptoLibTest and small clean-up

--- src/main/kotlin/Hotp.kt
+++ src/main/kotlin/Hotp.kt
@@ -20,8 +20,8 @@ class Hotp(
     }
 
     private fun dynamicTruncation(hmacResult : ByteArray): Int {
+        check(hmacResult.count() >= 20)
         val offset = (hmacResult.last() and 0xf).toInt()
-        // 31-bit, unsigned, big-endian integer; the first byte is masked with a 0x7f.
         val byte1 = (hmacResult[offset] and 0x7f).toInt() shl 24
         val byte2 = hmacResult[offset + 1].toUByte().toInt() shl 16
         val byte3 = hmacResult[offset + 2].toUByte().toInt() shl 8
